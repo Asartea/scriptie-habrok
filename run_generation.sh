221e118c6e13834823a7d71ad7ddd69976cf74dd
Joris van Bruggen <bruggenjoris@gmail.com>
2026-05-09 23:53:11 +0200

    WIP

--- run_generation.sh
+++ run_generation.sh
@@ -2,24 +2,22 @@
 #SBATCH --job-name=qwen_aoc_generation
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err
-#SBATCH --time=00:05:00
-#SBATCH --mem=4G
-#SBATCH --partition=regular
+#SBATCH --gres=gpu:2
+#SBATCH --time=04:00:00
+#SBATCH --mem=10G
+#SBATCH --partition=gpushort
 
 echo "Starting job on $(hostname)"
 echo "Time: $(date)"
 
-set -euxo pipefail
+set -e
 
-module load EESSI/2025.06
 module load Python/3.13.5-GCCcore-14.3.0
 module load uv
 
 cd ~/scriptie/habrok
 
 source .env
-pwd
-uv run test.py
-#uv run generation/main.py
+uv run generation/main.py
 
 echo "Finished at $(date)"
