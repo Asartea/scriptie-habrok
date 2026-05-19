40af9b3ecfb5a9821e9882053ea80022d0ca1379
Joris van Bruggen <bruggenjoris@gmail.com>
2026-05-19 22:33:18 +0200

    add new jobscript

---
+++ scripts/run_perplexity.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=aoc_generation
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+#SBATCH --gres=gpu:a100:2
+#SBATCH --time=04:00:00
+#SBATCH --mem=15G
+#SBATCH --partition=gpushort
+
+#!/bin/bash
+echo "Starting job on $(hostname)"
+echo "Time: $(date)"
+
+set -e
+
+module load Python/3.13.5-GCCcore-14.3.0
+module load uv
+
+cd ~/scriptie/habrok
+
+source .env
+uv run python3 -m detection.perplexity.fast_detect_gpt
+
+echo "Finished at $(date)"
